7fe2e2767436c7db452affa47068ceadab9a3d63
Greg Allen <greg@firstandthird.com>
2017-11-16 14:58:37 -0800

    tweak test method

--- test/methods/server.js
+++ test/methods/server.js
@@ -1,8 +1,5 @@
 module.exports = {
-  method: function() {
-    // 'this' should be server but appears to be server.plugins:
-    console.log('this should be the server:')
-    console.log(this);
+  method() {
     return this;
   }
 };
