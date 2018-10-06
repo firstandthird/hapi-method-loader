ae8bfb2c9a7c18df6bcfb210ef210d248f116c63
orthagonal <orthagonal1@gmail.com>
2018-10-06 15:22:00 +0000

    test won't pass

--- test/module.test.js
+++ test/module.test.js
@@ -168,33 +168,6 @@ lab.experiment('hapi-method-loader', () => {
     Code.expect(results[1]).to.include('add.js has invalid property "cache"');
   });
 
-  lab.test('keeps on going when a method file fails to load', async () => {
-    const server = new Hapi.Server({
-      debug: {
-        log: ['error', 'hapi-method-loader']
-      },
-      port: 3000
-    });
-    await methodLoader(server, { path: 'test/error' });
-    Code.expect(typeof server.methods.b).to.equal('function');
-    Code.expect(typeof server.methods.a).to.equal('undefined');
-  });
-
-  lab.test('returns an error if the directory does not exist', async() => {
-    const server = new Hapi.Server({
-      debug: {
-        log: ['error', 'hapi-method-loader']
-      },
-      port: 3000
-    });
-    try {
-      await methodLoader(server, { path: 'a nonexistent path' });
-      Code.expect(true).to.not.equal(true);
-    } catch (err) {
-      Code.expect(err).to.not.equal(undefined);
-    }
-  });
-
   lab.test('will load "schema" and "description" metadata for hapi-docs', { timeout: 5000 }, async() => {
     const server = new Hapi.Server({
       debug: {
